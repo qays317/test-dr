3a07824037963355fe22138d17637b5aa3e1fa40
qays-test <qaysalnajjad317@gmail.com>
2025-12-21 11:39:51 +0400

    Update config.sh

--- scripts/deployment-automation-scripts/config.sh
+++ scripts/deployment-automation-scripts/config.sh
@@ -39,4 +39,4 @@ CERTIFICATE_SANs='["*.rqays.com"]'
 ############################################
 PRIMARY_ALB_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-2:174512274809:certificate/80cf0123-dc9b-44ab-90d4-6c1f6bb1d951"
 DR_ALB_SSL_CERTIFICATE_ARN=""
-CLOUDFRONT_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-2:174512274809:certificate/80cf0123-dc9b-44ab-90d4-6c1f6bb1d951"
+CLOUDFRONT_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-1:174512274809:certificate/bfdb54d1-e12c-483e-9c4a-e5697af6c65d"
